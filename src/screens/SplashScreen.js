d471768b9c7969b3f18db0dd0dd0902f54cbfd73
Injila J <76965846+injl@users.noreply.github.com>
2024-02-18 05:22:45 +0500

    Style splash screen visuals
    - Added carousel on delay
    - Applied new fonts + styles

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from '../assets/stylesheet/styles';
 
@@ -7,11 +7,29 @@ const SplashScreen = ({ navigation }) => {
     // Navigate to the Home screen
     navigation.replace('Home');
   };
-  
+
+  const splashImages = [
+    require('../assets/images/literadraw-splash-1.png'),
+    require('../assets/images/literadraw-splash-2.png'),
+    require('../assets/images/literadraw-splash-3.png'),
+  ];
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Cycle through images at 600ms intervals
+      setCurrentImage((prevImage) => (prevImage + 1) % splashImages.length);
+    }, 600);
+
+    // Clear interval
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Image source={require('../assets/images/allwrite.png')} style={styles.logoLarge} />
-      <Text style={styles.h1}>Welcome to AllWrite.</Text>
+      <Image source={splashImages[currentImage]} style={styles.splashLogo} />
+      <Text style={styles.h1}>Welcome to LiteraDraw!</Text>
       <Text style={styles.body}>Literacy assessment for children, made easy through art.</Text>
       <TouchableOpacity style={styles.button} onPress={navigateToHome}>
           <Text style={styles.buttonText}>Get started →</Text>
