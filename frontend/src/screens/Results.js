4f80aab7a2e0768c63493883bb8b9cf1bb0e52e8
Injila J. <76965846+injl@users.noreply.github.com>
2024-05-06 05:46:36 +0500

    Update Extra Feedback to reflect recent commits
    Extra Feedback results cards now render in the same design as the Literacy Report results cards

--- frontend/src/screens/Results.js
+++ frontend/src/screens/Results.js
@@ -58,8 +58,8 @@ const ResultsScreen = ({ route }) => {
             {/* Conditionally render extra feedback */}
             {responseData.additional_info && responseData.additional_info.length > 0 && (
                 <>
-                    <Text style={[styles.resultsTitle, { marginTop: 10 }]}>Extra Feedback</Text>
-                    <Text style={[styles.body, { marginBottom: 20 }]}>
+                    <Text style={[styles.resultsTitle, { marginTop: 10 }]}>Additional Feedback</Text>
+                    <Text style={[styles.info, {textAlign: 'left', marginBottom: 20}]}>
                         Bonus: Here are your student's scores for the pre-writing shapes detected
                         in the drawing that are advanced for the {responseData.age_group} age group.
                     </Text>
@@ -68,12 +68,40 @@ const ResultsScreen = ({ route }) => {
             }
             {responseData.additional_info && responseData.additional_info.map((additional_info, index) => (
                 <View key={index} style={[styles.resultItem, { backgroundColor: '#EEF7E2' }]}>
-                    <Text style={[styles.shapeName]}>
-                        {/* Convert first letter to uppercase */}
-                        {additional_info.shape_name.charAt(0).toUpperCase() + additional_info.shape_name.slice(1)}
-                    </Text>
-                    <Text style={[styles.rating]}>Rating: {additional_info.rating}/2</Text>
-                    <Text style={[styles.feedback]}>{additional_info.feedback}</Text>
+                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                        {additional_info.shape_name == "circle" ? (
+                            <Image
+                            source={require('../assets/images/sampleCircle.png')}
+                            style={{width: 90, height: 90, marginLeft: 5, marginRight: 10}}
+                            />
+                        ) : additional_info.shape_name == "square" ? (
+                            <Image
+                            source={require('../assets/images/sampleSquare.png')}
+                            style={{width: 90, height: 90, marginLeft: 5, marginRight: 10}}
+                            />
+                        ) : additional_info.shape_name == "triangle" ? (
+                            <Image
+                            source={require('../assets/images/sampleTriangle.png')}
+                            style={{width: 90, height: 90, marginLeft: 5, marginRight: 10}}
+                            />
+                        ) : (
+                            <Text>Unknown shape</Text>
+                        )
+                    }
+                        <View style={{flexDirection: 'column'}}>
+                            <Text style={[styles.shapeName]}>
+                                {/* Convert first letter to uppercase */}
+                                {additional_info.shape_name.charAt(0).toUpperCase() + additional_info.shape_name.slice(1)}
+                            </Text>
+                            <Text style={[styles.rating]}>Rating: {additional_info.rating}/2</Text>
+                            <Text style={[styles.rating, {fontSize: 16, marginTop: 5, marginBottom: 10}]}>
+                                {additional_info.feedback[0]}
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={{flexDirection: 'column'}}>
+                        <Text style={[styles.feedback]}>{additional_info.feedback[1]}</Text>
+                    </View>
                 </View>
             ))}
         </ScrollView>
